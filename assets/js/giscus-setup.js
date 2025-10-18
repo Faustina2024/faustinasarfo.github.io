e645d2dec8c8afee450acd451740732f6ecd31ff
faustinasarfo <fserwaa88@gmail.com>
2025-10-18 02:19:06 +0000

    Deploying to gh-pages from @ faustinasarfo/faustinasarfo.github.io@6ef950f1a7d773f65e77eccdd0b2d3062815772d 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Faustina2024/faustinasarfo.github.io",
+    "data-repo": "faustinasarfo/faustinasarfo.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
